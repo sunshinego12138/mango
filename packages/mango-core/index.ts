93be98258ce46f44175a3f756f76209a28ae908f
李季 <2943102883@qq.com>
2025-02-23 17:18:07 +0800

    update

--- packages/mango-core/index.ts
+++ packages/mango-core/index.ts
@@ -20,7 +20,6 @@ import { Logestic } from 'logestic'
 export const init = (options: Mango.MongoStartOptions) => {
   optionsInit(options)
   const app = new Elysia()
-    .use(DBExtends)
     .use(controllerLoader(options))
     .decorate('env', loadEnv())
     .derive(({ store }: any) => {
@@ -55,33 +54,3 @@ export const init = (options: Mango.MongoStartOptions) => {
 
 export default { init }
 export * from './decorators'
-
-// export type App = ReturnType<typeof init>
-// // export type _Context = InferContext<App> & { [key: string]: any }
-// export type _Context = InferContext<App>
-// /** 每个方法的类型，这个目前没有用到 */
-// export type Handler = InferHandler<App>
-// // app.all方法第三个参数的类型
-// type AppAllType = ReturnType<typeof init>['all']
-// // 提取每个方法中第三个类型
-// export type ThirdParameterType = Parameters<AppAllType>[2]
-
-// /**
-//  * 上下文类型
-//  * 可以传递两个参数，用于间接的修改context中的类型
-//  * @example
-//  * @Get('/test', {
-//  *   query: querySchema,
-//  * })
-//  * test({ query }: Context<'query', typeof querySchema.static>) {
-//  *   return query
-//  * }
-//  *
-//  * @Get('/test2')
-//  * test2({ query }: Context) {
-//  *   return query
-//  * }
-//  */
-// export type Context<K extends keyof _Context = keyof _Context, V = any> = K extends keyof _Context
-//   ? Omit<_Context, K> & { [P in K]: V }
-//   : _Context
